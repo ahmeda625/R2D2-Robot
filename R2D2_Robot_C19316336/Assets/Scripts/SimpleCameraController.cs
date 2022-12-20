718822d4a13ba4eb4e5a253b7df7a03cb987b414
ahmeda625 <aalazawy625@gmail.com>
2022-12-20 16:20:06 +0000

    configured controller or mouse actions

--- R2D2_Robot_C19316336/Assets/Scripts/SimpleCameraController.cs
+++ R2D2_Robot_C19316336/Assets/Scripts/SimpleCameraController.cs
@@ -164,6 +164,138 @@ namespace UnityTemplateProjects
             return direction;
         }
 
-        
+        void Update()
+        {
+            // Exit Sample  
+
+            if (IsEscapePressed())
+            {
+                Application.Quit();
+				#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false; 
+				#endif
+            }
+
+            // Hide and lock cursor when right mouse button pressed
+            if (IsRightMouseButtonDown())
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+            // Unlock and show cursor when right mouse button released
+            if (IsRightMouseButtonUp())
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            // Rotation
+            if (IsCameraRotationAllowed())
+            {
+                var mouseMovement = GetInputLookRotation() * k_MouseSensitivityMultiplier * mouseSensitivity;
+                if (invertY)
+                    mouseMovement.y = -mouseMovement.y;
+                
+                var mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
+
+                m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
+                m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
+            }
+            
+            // Translation
+            var translation = GetInputTranslationDirection() * Time.deltaTime;
+
+            // Speed up movement when shift key held
+            if (IsBoostPressed())
+            {
+                translation *= 10.0f;
+            }
+            
+            // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
+            boost += GetBoostFactor();
+            translation *= Mathf.Pow(2.0f, boost);
+
+            m_TargetCameraState.Translate(translation);
+
+            // Framerate-independent interpolation
+            // Calculate the lerp amount, such that we get 99% of the way to our target in the specified time
+            var positionLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / positionLerpTime) * Time.deltaTime);
+            var rotationLerpPct = 1f - Mathf.Exp((Mathf.Log(1f - 0.99f) / rotationLerpTime) * Time.deltaTime);
+            m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
+
+            m_InterpolatingCameraState.UpdateTransform(transform);
+        }
+
+        float GetBoostFactor()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return boostFactorAction.ReadValue<Vector2>().y * 0.01f;
+#else
+            return Input.mouseScrollDelta.y * 0.01f;
+#endif
+        }
+
+        Vector2 GetInputLookRotation()
+        {
+            // try to compensate the diff between the two input systems by multiplying with empirical values
+#if ENABLE_INPUT_SYSTEM
+            var delta = lookAction.ReadValue<Vector2>();
+            delta *= 0.5f; // Account for scaling applied directly in Windows code by old input system.
+            delta *= 0.1f; // Account for sensitivity setting on old Mouse X and Y axes.
+            return delta;
+#else
+            return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+#endif
+        }
+
+        bool IsBoostPressed()
+        {
+#if ENABLE_INPUT_SYSTEM
+            bool boost = Keyboard.current != null ? Keyboard.current.leftShiftKey.isPressed : false; 
+            boost |= Gamepad.current != null ? Gamepad.current.xButton.isPressed : false;
+            return boost;
+#else
+            return Input.GetKey(KeyCode.LeftShift);
+#endif
+
+        }
+
+        bool IsEscapePressed()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return Keyboard.current != null ? Keyboard.current.escapeKey.isPressed : false; 
+#else
+            return Input.GetKey(KeyCode.Escape);
+#endif
+        }
+
+        bool IsCameraRotationAllowed()
+        {
+#if ENABLE_INPUT_SYSTEM
+            bool canRotate = Mouse.current != null ? Mouse.current.rightButton.isPressed : false;
+            canRotate |= Gamepad.current != null ? Gamepad.current.rightStick.ReadValue().magnitude > 0 : false;
+            return canRotate;
+#else
+            return Input.GetMouseButton(1);
+#endif
+        }
+
+        bool IsRightMouseButtonDown()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return Mouse.current != null ? Mouse.current.rightButton.isPressed : false;
+#else
+            return Input.GetMouseButtonDown(1);
+#endif
+        }
+
+        bool IsRightMouseButtonUp()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return Mouse.current != null ? !Mouse.current.rightButton.isPressed : false;
+#else
+            return Input.GetMouseButtonUp(1);
+#endif
+        }
     }
 }
