f45228e7d172fc6190f43cea071d193db1290813
ahmeda625 <aalazawy625@gmail.com>
2022-12-20 16:16:48 +0000

    start method

--- R2D2_Robot_C19316336/Assets/Scripts/SimpleCameraController.cs
+++ R2D2_Robot_C19316336/Assets/Scripts/SimpleCameraController.cs
@@ -53,5 +53,117 @@ namespace UnityTemplateProjects
                 t.position = new Vector3(x, y, z);
             }
         }
+
+        const float k_MouseSensitivityMultiplier = 0.01f;
+
+        CameraState m_TargetCameraState = new CameraState();
+        CameraState m_InterpolatingCameraState = new CameraState();
+
+        [Header("Movement Settings")]
+        [Tooltip("Exponential boost factor on translation, controllable by mouse wheel.")]
+        public float boost = 3.5f;
+
+        [Tooltip("Time it takes to interpolate camera position 99% of the way to the target."), Range(0.001f, 1f)]
+        public float positionLerpTime = 0.2f;
+
+        [Header("Rotation Settings")]
+        [Tooltip("Multiplier for the sensitivity of the rotation.")]
+        public float mouseSensitivity = 60.0f;
+
+        [Tooltip("X = Change in mouse position.\nY = Multiplicative factor for camera rotation.")]
+        public AnimationCurve mouseSensitivityCurve = new AnimationCurve(new Keyframe(0f, 0.5f, 0f, 5f), new Keyframe(1f, 2.5f, 0f, 0f));
+
+        [Tooltip("Time it takes to interpolate camera rotation 99% of the way to the target."), Range(0.001f, 1f)]
+        public float rotationLerpTime = 0.01f;
+
+        [Tooltip("Whether or not to invert our Y axis for mouse input to rotation.")]
+        public bool invertY = false;
+
+#if ENABLE_INPUT_SYSTEM
+        InputAction movementAction;
+        InputAction verticalMovementAction;
+        InputAction lookAction;
+        InputAction boostFactorAction;
+        bool        mouseRightButtonPressed;
+
+        void Start()
+        {
+            var map = new InputActionMap("Simple Camera Controller");
+
+            lookAction = map.AddAction("look", binding: "<Mouse>/delta");
+            movementAction = map.AddAction("move", binding: "<Gamepad>/leftStick");
+            verticalMovementAction = map.AddAction("Vertical Movement");
+            boostFactorAction = map.AddAction("Boost Factor", binding: "<Mouse>/scroll");
+
+            lookAction.AddBinding("<Gamepad>/rightStick").WithProcessor("scaleVector2(x=15, y=15)");
+            movementAction.AddCompositeBinding("Dpad")
+                .With("Up", "<Keyboard>/w")
+                .With("Up", "<Keyboard>/upArrow")
+                .With("Down", "<Keyboard>/s")
+                .With("Down", "<Keyboard>/downArrow")
+                .With("Left", "<Keyboard>/a")
+                .With("Left", "<Keyboard>/leftArrow")
+                .With("Right", "<Keyboard>/d")
+                .With("Right", "<Keyboard>/rightArrow");
+            verticalMovementAction.AddCompositeBinding("Dpad")
+                .With("Up", "<Keyboard>/pageUp")
+                .With("Down", "<Keyboard>/pageDown")
+                .With("Up", "<Keyboard>/e")
+                .With("Down", "<Keyboard>/q")
+                .With("Up", "<Gamepad>/rightshoulder")
+                .With("Down", "<Gamepad>/leftshoulder");
+            boostFactorAction.AddBinding("<Gamepad>/Dpad").WithProcessor("scaleVector2(x=1, y=4)");
+
+            movementAction.Enable();
+            lookAction.Enable();
+            verticalMovementAction.Enable();
+            boostFactorAction.Enable();
+        }
+#endif
+
+        void OnEnable()
+        {
+            m_TargetCameraState.SetFromTransform(transform);
+            m_InterpolatingCameraState.SetFromTransform(transform);
+        }
+
+        Vector3 GetInputTranslationDirection()
+        {
+            Vector3 direction = Vector3.zero;
+#if ENABLE_INPUT_SYSTEM
+            var moveDelta = movementAction.ReadValue<Vector2>();
+            direction.x = moveDelta.x;
+            direction.z = moveDelta.y;
+            direction.y = verticalMovementAction.ReadValue<Vector2>().y;
+#else
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction += Vector3.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                direction += Vector3.back;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction += Vector3.left;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                direction += Vector3.right;
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                direction += Vector3.down;
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                direction += Vector3.up;
+            }
+#endif
+            return direction;
+        }
+
+        
     }
 }
