18e3b0ab30206603dd4938f6480716727fce32de
ahmeda625 <aalazawy625@gmail.com>
2022-12-20 14:00:17 +0000

    setup camera follow settings and initialised variables

--- R2D2_Robot_C19316336/Assets/Scripts/CameraFollow.cs
+++ R2D2_Robot_C19316336/Assets/Scripts/CameraFollow.cs
@@ -6,5 +6,10 @@ using UnityEngine;
 /// </summary>
 public class CameraFollow : MonoBehaviour
 {
+    [Header("Follow Settings")]
+    [Tooltip("By default, the camera will precisely follow the robot's position. This vector will offset that position as needed.")]
+    [SerializeField] private Vector3 followOffset = new Vector3(0f, 1f, -2.6f);
+    private Transform robotTransform;
 
+    
 }
