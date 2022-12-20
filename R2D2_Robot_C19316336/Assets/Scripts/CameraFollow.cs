7eb6d8254b4c2522ded55080dc0a4e69776f9281
ahmeda625 <aalazawy625@gmail.com>
2022-12-20 13:53:33 +0000

    setup scripts folder and added camerafollow.cs file

---
+++ R2D2_Robot_C19316336/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Makes sure that the camera follows a certain object (e.g the robot). Supports an offset.
+/// </summary>
+public class CameraFollow : MonoBehaviour
+{
+
+}
